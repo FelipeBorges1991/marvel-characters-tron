c0b6601d49845d60ad4193fd747d4ee3d7a78f8b
Felipe Borges <fcborges_@hotmail.com>
2021-06-09 21:36:09 -0300

    Organizando componentes e rotas

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import HomePage from "../routeComponents/HomePage";
+
+function App() {
+  const [charactersList, setCharacter] = useState(false);
+
+  return <div className="App">Marvel</div>;
+}
+
+export default App;
---
+++ src/routeComponents/HomePage.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Homepage() {
+    const [charactersList, setCharacters] = useState(false);
+    
+  
+    return (
+    
+    );
+  }
+  
+  export default Homepage;
