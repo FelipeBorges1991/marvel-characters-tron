2f1cda05c4a7a566fc11aa25286c16e08d623aaa
Felipe Borges <fcborges_@hotmail.com>
2021-06-10 16:30:01 -0300

    Conexão da API / atualização dos componentes Homepage e App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios'
-// import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// import HomePage from "../routeComponents/HomePage";
+import Homepage from "../routeComponents/HomePage";
 
 function App() {
-  const [charactersList, setCharacter] = useState(false);
-
-  return <div className="App">Marvel</div>;
+  return (
+    <BrowserRouter>
+      <div className="vh-100">
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/routeComponents/HomePage.js
+++ src/routeComponents/HomePage.js
@@ -1,13 +1,37 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import md5 from "md5";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const publicKey = "c7dbb9aaf046c6e7dc40db5aebfffabd";
+const privateKey = "94b44e6503b2e1660c4e2136da55279f3c61d314";
+
+const time = Number(new Date());
+
+const hash = md5(time + privateKey + publicKey);
 
 function Homepage() {
-    const [charactersList, setCharacters] = useState(false);
-    
-  
-    return (
-    
-    );
-  }
-  
-  export default Homepage;
+  const [characterslist, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCharacters() {
+      try {
+        const response = await axios.get(
+          `http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`
+        );
+
+        setCharacters([response.data.data]);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+        setLoading(false);
+      }
+    }
+    fetchCharacters();
+  }, []);
+
+  return <div>Marvel</div>;
+}
+
+export default Homepage;
